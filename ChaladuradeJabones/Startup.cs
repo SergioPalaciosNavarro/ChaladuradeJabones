7d1c6c45c6c4c7935666e9f7706f8a212e06ac05
epalacio <epalacio@LES004666>
2017-10-20 12:39:53 +0200

    Proyecto

---
+++ ChaladuradeJabones/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChaladuradeJabones.Startup))]
+namespace ChaladuradeJabones
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
